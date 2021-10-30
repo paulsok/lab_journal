f4e4f092ac5c3d4a11a336539c95903d4463a9e2
paulsok <paul.s.falke@gmail.com>
2021-10-30 20:33:51 +0200

    topics section

--- lab_journals/urls.py
+++ lab_journals/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     # home page
     url(r'^$', views.index, name='index'),
+    # show all topics
+    url(r'^topics/$', views.topics, name='topics'),
+    # detail page for a single topic
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]

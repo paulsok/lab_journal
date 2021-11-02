9ad1e49dd99b817f8359eea545389ba19848a272
paulsok <paul.s.falke@gmail.com>
2021-11-02 16:16:05 +0100

    users logout and register logic

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+"""Defines URL patterns for users"""
+from django.conf.urls import url
+from django.contrib.auth.views import LoginView
+from . import views
+
+urlpatterns = [url(r'^login/$', LoginView.as_view(template_name='login.html'), name='login'),
+               url(r'^logout/$', views.logout_view, name='logout'),
+               url(r'^register/$', views.register, name='register')
+]

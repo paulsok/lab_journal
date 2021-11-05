88dcfe9d6ce398f579c7ff3cd03b52cd2e026283
paulsok <paul.s.falke@gmail.com>
2021-11-05 13:17:52 +0100

    front page fix

--- lab_journal/settings.py
+++ lab_journal/settings.py
@@ -15,6 +15,7 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = 'lab_journals/static'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
